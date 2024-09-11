9cb1088024301e05c1abd97f05989d1607353ce8
CQUT-rjgc-zys <2200642826@qq.com>
2024-09-12 07:39:01 +0800

    init

---
+++ src/main/java/com/curriculumdesign/drugtraceabilitysystem/DrugTraceabilitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.curriculumdesign.drugtraceabilitysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugTraceabilitySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrugTraceabilitySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curriculumdesign/drugtraceabilitysystem/DrugTraceabilitySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curriculumdesign.drugtraceabilitysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugTraceabilitySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
